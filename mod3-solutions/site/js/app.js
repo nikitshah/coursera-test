e7f152b3cf2c0bb79171ffeab0ce7b0d6b8831c9
Nikit Shah <Nikit Shah>
2016-08-08 02:31:09 -0700

    coursera assignment 3

---
+++ mod3-solutions/site/js/app.js
@@ -0,0 +1,58 @@
+var main = function() {
+    $('.article').click(function() {
+    $('.article').removeClass('current');
+    $('.description').hide();
+
+    $(this).addClass('current');
+    $(this).children('.description').show();
+  });
+
+    $('.arrow-next').click(function() {
+    var currentSlide = $('.active-slide');
+    var nextSlide = currentSlide.next();
+
+    var currentDot = $('.active-dot');
+    var nextDot = currentDot.next();
+
+    if(nextSlide.length === 0) {
+      nextSlide = $('.slide').first();
+      nextDot = $('.dot').first();
+    }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    nextSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+  });
+
+
+  $('.arrow-prev').click(function() {
+    var currentSlide = $('.active-slide');
+    var prevSlide = currentSlide.prev();
+
+    var currentDot = $('.active-dot');
+    var prevDot = currentDot.prev();
+
+    if(prevSlide.length === 0) {
+      prevSlide = $('.slide').last();
+      prevDot = $('.dot').last();
+    }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    prevSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    prevDot.addClass('active-dot');
+  });
+
+  $('#screen').click(function(){
+    
+   $('#video').addClass('no');
+
+
+
+  });
+}
+
+$(document).ready(main);
